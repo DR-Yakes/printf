367b1d04efda04e8da78ebaf77d793c8598aee93
DR-Yakes <yakesinc@gmail.com>
2022-10-20 20:51:43 +0000

    updated main.h

--- main.h
+++ main.h
@@ -36,5 +36,40 @@ int print_unsigned(va_list 1, flags_t *f);
 int count_digit(int i);
 
 /* print_bases */
+int print_hex(va_list 1, flags_t *f);
+int print_hex_big(va_list 1, flags_t *f);
+int print_binary(va_list 1, flags_t *f);
+int print_octal(va_list 1, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *f);
+
+/*get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list 1, flags_t *f);
+int print_char(va_list 1, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list 1, flags_t *f);
+int print_rev(va_list 1, flags_t *f);
+int print_bigS(va_list 1, flags_t *f);
+
+/* print percent */
+int print_percent(va_list 1, flags_t *f);
+
+/* print address */
+int print_address(va_lists 1, flags_t *f);
 
 #endif
